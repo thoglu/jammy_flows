0543569a913098772b8d3aafa2a8ad4075dc4045
thoglu <thorsten.glu@gmail.com>
2022-01-14 20:45:50 +0100

    fixed tests and linting

--- tests/test_skewness.py
+++ tests/test_skewness.py
@@ -40,7 +40,7 @@ def check_conditional_pdf(num_kde, plot=False, index=0, dimensionality=3):
     print("X ", x)
     #bw=(x[1]-x[0]).numpy()
 
-    exponents=torch.rand( (dimensionality, num_kde, 2) , dtype=torch.double)*2+0.1
+    exponents=torch.rand( (dimensionality, num_kde, 2) , dtype=torch.double)*2-2
 
     print(" NUM KDE", num_kde, "INDEX ", index)
     log_widths=torch.rand( (dimensionality, num_kde, 2) , dtype=torch.double)*2+0.1
@@ -48,13 +48,10 @@ def check_conditional_pdf(num_kde, plot=False, index=0, dimensionality=3):
     log_norms=torch.rand( (dimensionality, num_kde, 2) , dtype=torch.double)*2+0.1
     means=torch.rand( (dimensionality, num_kde, 2) , dtype=torch.double)*5-5
 
-    skew_signs=pdf.layer_list[0][0].skew_signs
+    skew_signs=pdf.layer_list[0][0].kde_skew_signs
 
-    total_cdf=pdf.layer_list[0][0].logistic_kernel_log_cdf(x, means, log_widths, log_norms, exponents, skew_signs).squeeze(-1).exp().numpy()
-    total_sf=pdf.layer_list[0][0].logistic_kernel_log_sf(x, means, log_widths, log_norms, exponents, skew_signs).squeeze(-1).exp().numpy()
-    #total_pdf=pdf.layer_list[0][0].logistic_kernel_log_pdf((x-0.5*bw).reshape(-1,1), means, log_widths, log_norms, exponents, skew_signs).squeeze(-1).exp().numpy()
-    total_pdf_right=pdf.layer_list[0][0].logistic_kernel_log_pdf((x), means, log_widths, log_norms, exponents, skew_signs).squeeze(-1).exp().numpy()
-
+    total_cdf, total_sf, total_pdf=pdf.layer_list[0][0].logistic_kernel_log_pdf_quantities(x, means, log_widths, log_norms, exponents, skew_signs)
+  
 
 def check_skewness(num_kde, plot=False, index=0):
 
@@ -71,7 +68,7 @@ def check_skewness(num_kde, plot=False, index=0):
 
     bw=(x[1]-x[0]).numpy()
 
-    exponents=torch.rand( (1, num_kde, 1) , dtype=torch.double)*2+0.1
+    log_exponents=torch.rand( (1, num_kde, 1) , dtype=torch.double)*2-1
 
     print(" NUM KDE", num_kde, "INDEX ", index)
     log_widths=torch.rand( (1, num_kde, 1) , dtype=torch.double)*2+0.1
@@ -79,23 +76,26 @@ def check_skewness(num_kde, plot=False, index=0):
     log_norms=torch.rand( (1, num_kde, 1) , dtype=torch.double)*2+0.1
     means=torch.rand( (1, num_kde, 1) , dtype=torch.double)*5-5
 
-    skew_signs=pdf.layer_list[0][0].skew_signs
+    skew_signs=pdf.layer_list[0][0].kde_skew_signs
 
 
-    total_cdf=pdf.layer_list[0][0].logistic_kernel_log_cdf(x.reshape(-1,1), means, log_widths, log_norms, exponents, skew_signs).squeeze(-1).exp().numpy()
-    total_sf=pdf.layer_list[0][0].logistic_kernel_log_sf(x.reshape(-1,1), means, log_widths, log_norms, exponents, skew_signs).squeeze(-1).exp().numpy()
-    total_pdf=pdf.layer_list[0][0].logistic_kernel_log_pdf((x-0.5*bw).reshape(-1,1), means, log_widths, log_norms, exponents, skew_signs).squeeze(-1).exp().numpy()
-    total_pdf_right=pdf.layer_list[0][0].logistic_kernel_log_pdf((x+0.5*bw).reshape(-1,1), means, log_widths, log_norms, exponents, skew_signs).squeeze(-1).exp().numpy()
+    total_cdf, total_sf, _=pdf.layer_list[0][0].logistic_kernel_log_pdf_quantities(x.reshape(-1,1), means, log_widths, log_norms, log_exponents, skew_signs)
+    _, _, total_pdf=pdf.layer_list[0][0].logistic_kernel_log_pdf_quantities((x-0.5*bw).reshape(-1,1), means, log_widths, log_norms, log_exponents, skew_signs)
+    total_sf=total_sf.squeeze(-1).exp().numpy()
+    total_cdf=total_cdf.squeeze(-1).exp().numpy()
+    total_pdf=total_pdf.squeeze(-1).exp().numpy()
 
+    _, _, total_pdf_right=pdf.layer_list[0][0].logistic_kernel_log_pdf_quantities((x+0.5*bw).reshape(-1,1), means, log_widths, log_norms, log_exponents, skew_signs)
+    total_pdf_right=total_pdf_right.squeeze(-1).exp().numpy()
 
     numerical_cdf=numpy.cumsum(total_pdf)*bw
     numerical_sf=(numpy.cumsum(total_pdf_right[::-1])*bw)[::-1]
 
-    assert( numpy.fabs(total_cdf-numerical_cdf).max() <1e-4) , ("MAX DIFF CDF num/exact: ", numpy.fabs(total_cdf-numerical_cdf).max())
-    #assert( numpy.fabs(total_sf-numerical_sf).max() <1e-5), ("MAX DIFF SF num/exact: ", numpy.fabs(total_sf-numerical_sf).max()) 
+    assert( numpy.fabs(total_cdf-numerical_cdf).max() <1e-5) , ("MAX DIFF CDF num/exact: ", numpy.fabs(total_cdf-numerical_cdf).max())
+    assert( numpy.fabs(total_sf-numerical_sf).max() <1e-5), ("MAX DIFF SF num/exact: ", numpy.fabs(total_sf-numerical_sf).max()) 
 
-    print("MAX DIFF CDF", numpy.fabs(total_cdf-numerical_cdf).max())
-    print("MAX DIFF SF", numpy.fabs(total_sf-numerical_sf).max())
+    #print("MAX DIFF CDF", numpy.fabs(total_cdf-numerical_cdf).max())
+    #print("MAX DIFF SF", numpy.fabs(total_sf-numerical_sf).max())
     if(plot):   
         fig=pylab.figure()
 
@@ -117,11 +117,13 @@ def check_skewness(num_kde, plot=False, index=0):
         total_norm_sum=log_norms.exp().sum().numpy()
         for cur_kde in range(num_kde):
 
-            cur_pdf=pdf.layer_list[0][0].logistic_kernel_log_pdf((x-0.5*bw).reshape(-1,1), means[:,cur_kde:cur_kde+1,:], log_widths[:,cur_kde:cur_kde+1,:], log_norms[:,cur_kde:cur_kde+1,:], exponents[:,cur_kde:cur_kde+1,:],skew_signs[:,cur_kde:cur_kde+1,:]).squeeze(-1).exp().numpy()
+            _,_,cur_pdf=pdf.layer_list[0][0].logistic_kernel_log_pdf_quantities((x-0.5*bw).reshape(-1,1), means[:,cur_kde:cur_kde+1,:], log_widths[:,cur_kde:cur_kde+1,:], log_norms[:,cur_kde:cur_kde+1,:], log_exponents[:,cur_kde:cur_kde+1,:],skew_signs[:,cur_kde:cur_kde+1,:])
+            cur_pdf=cur_pdf.squeeze(-1).exp().numpy()
+
             numerical_total_pdf+=cur_pdf*log_norms[0,cur_kde,0].exp().numpy()
             cur_sign=skew_signs[0,cur_kde, 0].numpy()
-            cur_exponent=exponents[0,cur_kde,0].numpy()
-            ax2.plot(x, cur_pdf, label="weighted pdf %.2d (%d/%.1f)" % (cur_kde, cur_sign,cur_exponent))
+            cur_exponent=numpy.exp(log_exponents[0,cur_kde,0].numpy())
+            ax2.plot(x, cur_pdf, label="weighted pdf %.2d (%d/%.2f)" % (cur_kde, cur_sign,cur_exponent))
 
         numerical_total_pdf/=total_norm_sum
         ax2.plot(x, total_pdf, color="black", label="tot pdf")
