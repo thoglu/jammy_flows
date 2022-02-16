added rotation options to gf layer, restructuring + minor bug fixes

jammy_flows/flows.py
@@ -196,7 +196,7 @@ class pdf(nn.Module):
         self.flow_dict["g"]["kwargs"]["width_smooth_saturation"]=1 # 
         self.flow_dict["g"]["kwargs"]["regulate_normalization"]=1
         self.flow_dict["g"]["kwargs"]["add_skewness"]=0
-
+        self.flow_dict["g"]["kwargs"]["rotation_mode"]="householder"
 
         self.flow_dict["h"] = dict()
         self.flow_dict["h"]["module"] = gf_block_old
@@ -253,8 +253,9 @@ class pdf(nn.Module):
         self.flow_dict["m"]["kwargs"]["use_permanent_parameters"]=0
         self.flow_dict["m"]["kwargs"]["use_extra_householder"] = 0
         self.flow_dict["m"]["kwargs"]["euclidean_to_sphere_as_first"] = 0
-        self.flow_dict["m"]["kwargs"]["num_moebius"] = 5
+        self.flow_dict["m"]["kwargs"]["num_basis_functions"] = 5
         self.flow_dict["m"]["kwargs"]["natural_direction"] = 0
+        self.flow_dict["m"]["kwargs"]["use_splines"] = 0
 
         """
         S2 flows
@@ -266,7 +267,7 @@ class pdf(nn.Module):
         self.flow_dict["n"]["kwargs"]["use_permanent_parameters"]=0
         self.flow_dict["n"]["kwargs"]["use_extra_householder"] = 1
         self.flow_dict["n"]["kwargs"]["euclidean_to_sphere_as_first"] = 0
-        self.flow_dict["n"]["kwargs"]["num_moebius"] = 10
+        self.flow_dict["n"]["kwargs"]["num_basis_functions"] = 10
         self.flow_dict["n"]["kwargs"]["higher_order_cylinder_parametrization"] = True
         self.flow_dict["n"]["kwargs"]["zenith_type_layers"] = "r"
         self.flow_dict["n"]["kwargs"]["max_rank"] = -1
@@ -1417,8 +1418,8 @@ class pdf(nn.Module):
             data_type = data_summary.dtype
             used_device = data_summary.device
 
-            assert(data_summary.dim()==2)
-            assert(data_summary.shape[0]==1), ("Requiring batćh size of 1! .. having .. ", data_summary.shape[0])
+            assert(data_summary.dim()==2), data_summary
+            #assert(data_summary.shape[0]==1), ("Requiring batćh size of 1! .. having .. ", data_summary.shape[0])
 
         x=None
         log_gauss_evals=0.0

jammy_flows/helper_fns.py
@@ -204,7 +204,7 @@ def get_pdf_on_grid(mins_maxs, npts, model, conditional_input=None, s2_norm="sta
       print((numpy.isfinite(res)==False).sum())
       print(numpy_positions[(numpy.isfinite(res)==False)])
 
-      r,_,_=model(eval_positions[mask_inner][torch.isfinite(log_res)==False][:1], conditional_input=cinput)
+      r,_,_=model(eval_positions[mask_inner][torch.isfinite(log_res)==False][:], conditional_input=cinput[torch.isfinite(log_res)==False])
       print(r)
       raise Exception()
 

jammy_flows/layers/euclidean/multivariate_normal.py
@@ -5,7 +5,7 @@ import numpy
 from .. import bisection_n_newton as bn
 from .. import layer_base
 from ... import extra_functions
-
+from .. import matrix_fns
 from . import euclidean_base
 
 import math
@@ -182,150 +182,6 @@ class mvn_block(euclidean_base.euclidean_base):
 
         #######################################
 
-    def obtain_transformation_matrix_and_logdet(self, single_log_diagonal_entry=None, log_diagonal_entries=None, lower_triangular_entries=None, cov_type="full", upper_triangular=False):
-        """
-        Obtain lower trinagular (cholesky) matrix. Diagonal entries have to be positive
-        """
-        ## all variances are the same
-        if(cov_type=="unit_gaussian"):
-
-            return torch.eye(self.dimension, dtype=torch.float64).unsqueeze(0), 0.0
-
-        elif(cov_type=="diagonal_symmetric"):
-            assert(single_log_diagonal_entry is not None)
-            assert(single_log_diagonal_entry.shape[1]==1)
-
-            full_diagonal=single_log_diagonal_entry.repeat(1, self.dimension)
-
-            return torch.diag_embed(full_diagonal.exp()), self.dimension*single_log_diagonal_entry.sum(axis=-1)
-
-        elif(cov_type=="diagonal"):
-            assert(log_diagonal_entries is not None)
-            assert(log_diagonal_entries.shape[1]==self.dimension)
-
-            return torch.diag_embed(torch.exp(log_diagonal_entries)), log_diagonal_entries.sum(axis=-1)
-
-        elif(cov_type=="full"):
-
-            assert(log_diagonal_entries is not None)
-
-            assert(lower_triangular_entries.shape[1]==int(self.dimension*(self.dimension-1)/2) )
-
-            cum_indices=numpy.cumsum(numpy.arange(self.dimension)+1)
-            tot_output=0.0
-
-            for ind in range(self.dimension):
-
-                offset=-self.dimension+ind+1
-
-                if(ind==(self.dimension-1)):
-                    ## diagonal
-                    tot_output=tot_output+torch.diag_embed(torch.exp(log_diagonal_entries))
-                else:
-
-                    if(ind==0):
-                        ## add 
-                        tot_output=tot_output+torch.diag_embed(lower_triangular_entries[:,:cum_indices[ind]], offset=offset)
-                    else:   
-                        tot_output=tot_output+torch.diag_embed(lower_triangular_entries[:, cum_indices[ind-1]:cum_indices[ind]], offset=offset)
-
-            if(upper_triangular):
-                # upper triangular matrix is just transpose of lower triangular
-                tot_output=tot_output.permute(0, 2,1)
-
-            return tot_output, log_diagonal_entries.sum(axis=-1)
-
-        else:
-            raise Exception("Unknown cov type", cov_type)
-
-    def obtain_inverse_transformation_matrix_and_logdet(self, single_log_diagonal_entry=None, log_diagonal_entries=None, lower_triangular_entries=None, cov_type="full", upper_triangular=False):
-
-        if(cov_type=="unit_gaussian"):
-
-            return torch.eye(self.dimension, dtype=torch.float64).unsqueeze(0), 0.0
-
-        elif(cov_type=="diagonal_symmetric"):
-            assert(single_log_diagonal_entry is not None)
-            assert(single_log_diagonal_entry.shape[1]==1)
-
-            full_diagonal=(torch.exp(-single_log_diagonal_entry)).repeat(1, self.dimension)
-
-            return torch.diag_embed(full_diagonal), -self.dimension*single_log_diagonal_entry.sum(axis=-1)
-
-        elif(cov_type=="diagonal"):
-            assert(log_diagonal_entries is not None)
-            assert(log_diagonal_entries.shape[1]==self.dimension)
-
-            return torch.diag_embed(torch.exp(-log_diagonal_entries)), -log_diagonal_entries.sum(axis=-1)
-
-        elif(cov_type=="full"):
-
-            assert(log_diagonal_entries is not None)
-            assert(lower_triangular_entries is not None)
-
-            cum_indices=numpy.cumsum(numpy.arange(self.dimension)+1)
-            tot_output=0.0
-
-            ## obtain regular matrix .. determinantes of sub matrices are used for inverse
-            regular_matrix,_=self.obtain_transformation_matrix_and_logdet(log_diagonal_entries=log_diagonal_entries, lower_triangular_entries=lower_triangular_entries, cov_type="full")
-            
-
-            largest_submatrix=regular_matrix[:, 1:,:-1]
-
-            for ind in range(self.dimension):
-
-                # ind= 0 -> offset = dim-1
-                # ind =1 -> offset = dim-2 ...
-                # ind = dim-1 -> offset = 0
-
-                offset=-self.dimension+ind+1
-
-                if(ind==(self.dimension-1)):
-                    ## diagonal
-                    tot_output=tot_output+torch.diag_embed(torch.exp(-log_diagonal_entries))
-                else:
-
-                    use_sign_flip=int(numpy.fabs(offset))%2
-
-                    sign_factor=1.0
-                    if(use_sign_flip==1):
-                        sign_factor=-1.0
-
-                    ## inverse off diagonal entries are sub-determinants divided by comibnations of diagonal
-                    if(ind==0):
-                        ## the first 
-                        #print("IND 0")
-                       
-                        
-                        entry=(torch.det(largest_submatrix).unsqueeze(-1))/torch.exp(torch.sum(log_diagonal_entries, dim=1, keepdims=True))
-
-                        tot_output=tot_output+torch.diag_embed(entry*sign_factor, offset=offset)
-
-                    else:   
-
-                        off_diagonals=[]
-                        num_off_diagonals=ind+1
-                        largest_submatrix_subdim=self.dimension-1
-
-                        individual_matrices_subdim=largest_submatrix_subdim-ind
-
-                        for sub_ind in range(num_off_diagonals):
-                            
-                            off_diagonals.append(sign_factor*(torch.det(largest_submatrix[:, sub_ind:sub_ind+individual_matrices_subdim, sub_ind:sub_ind+individual_matrices_subdim]).unsqueeze(-1))/torch.exp(torch.sum(log_diagonal_entries[:, sub_ind:sub_ind+individual_matrices_subdim+1], dim=1, keepdims=True) ) )
-                            
-                        off_diagonals=torch.cat(off_diagonals, dim=1)
-                        tot_output=tot_output+torch.diag_embed(off_diagonals, offset=offset)
-
-
-            if(upper_triangular):
-                tot_output=tot_output.permute(0,2,1)
-
-         
-            return tot_output, -log_diagonal_entries.sum(axis=-1)
-
-        else:
-            raise Exception("Unknown cov type", cov_type)
-
 
     def _obtain_usable_flow_params(self, x, cov_type, extra_inputs=None):
 
@@ -379,7 +235,7 @@ class mvn_block(euclidean_base.euclidean_base):
         single_log_diagonal, full_log_diagonal, lower_triangular_entries=self._obtain_usable_flow_params(z, self.cov_type, extra_inputs=extra_inputs)
 
         # use inverse of lower transformation matrix
-        trafo_matrix, extra_logdet=self.obtain_inverse_transformation_matrix_and_logdet(single_log_diagonal_entry=single_log_diagonal, log_diagonal_entries=full_log_diagonal, lower_triangular_entries=lower_triangular_entries, cov_type=self.cov_type)
+        trafo_matrix, extra_logdet=matrix_fns.obtain_lower_triangular_matrix_and_logdet(self.dimension, single_log_diagonal_entry=single_log_diagonal, log_diagonal_entries=full_log_diagonal, lower_triangular_entries=lower_triangular_entries, cov_type=self.cov_type)
 
         res=torch.einsum("...ij, ...j", trafo_matrix, z)
         log_det=log_det+extra_logdet
@@ -397,7 +253,7 @@ class mvn_block(euclidean_base.euclidean_base):
         single_log_diagonal, full_log_diagonal, lower_triangular_entries=self._obtain_usable_flow_params(x, self.cov_type, extra_inputs=extra_inputs)
 
         ## normally the inverse mapping involves the Upper trinagular matrix .. but we can just a well work with the lower triangular one, which just perumtes the dimensions
-        trafo_matrix, extra_logdet=self.obtain_transformation_matrix_and_logdet(single_log_diagonal_entry=single_log_diagonal, log_diagonal_entries=full_log_diagonal, lower_triangular_entries=lower_triangular_entries, cov_type=self.cov_type)
+        trafo_matrix, extra_logdet=matrix_fns.obtain_inverse_lower_triangular_matrix_and_logdet(self.dimension, single_log_diagonal_entry=single_log_diagonal, log_diagonal_entries=full_log_diagonal, lower_triangular_entries=lower_triangular_entries, cov_type=self.cov_type)
 
         #res=torch.bmm(trafo_matrix, x)
         res=torch.einsum("...ij, ...j", trafo_matrix, x)

jammy_flows/layers/intervals/rational_quadratic_spline.py
@@ -4,15 +4,10 @@ from torch.nn import functional as F
 
 import numpy as np
 
+from .. import spline_fns
 from . import interval_base
 
-def searchsorted(bin_locations, inputs, eps=1e-6):
-    bin_locations[..., -1] += eps
-    return torch.sum(
-        inputs >= bin_locations,
-        dim=-1,
-        keepdims=True
-    ) - 1
+
 
 """
 This layer implements neural spline flows (https://arxiv.org/abs/1906.04032) via monotonic rational-quadratic splines. 
@@ -28,8 +23,8 @@ def unconstrained_rational_quadratic_spline(inputs,
                                             inverse=False,
                                             tails='linear',
                                             tail_bound=1.,
-                                            min_bin_width=DEFAULT_MIN_BIN_WIDTH,
-                                            min_bin_height=DEFAULT_MIN_BIN_HEIGHT,
+                                            rel_min_bin_width=DEFAULT_MIN_BIN_WIDTH,
+                                            rel_min_bin_height=DEFAULT_MIN_BIN_HEIGHT,
                                             min_derivative=DEFAULT_MIN_DERIVATIVE):
     inside_interval_mask = (inputs >= -tail_bound) & (inputs <= tail_bound)
     outside_interval_mask = ~inside_interval_mask
@@ -55,8 +50,8 @@ def unconstrained_rational_quadratic_spline(inputs,
         unnormalized_derivatives=unnormalized_derivatives[inside_interval_mask, :],
         inverse=inverse,
         left=-tail_bound, right=tail_bound, bottom=-tail_bound, top=tail_bound,
-        min_bin_width=min_bin_width,
-        min_bin_height=min_bin_height,
+        rel_min_bin_width=rel_min_bin_width,
+        rel_min_bin_height=rel_min_bin_height,
         min_derivative=min_derivative
     )
 
@@ -104,124 +99,7 @@ class rational_quadratic_spline(interval_base.interval_base):
         self.min_derivative=min_derivative
 
 
-    def rational_quadratic_spline(self,
-                              inputs,
-                              unnormalized_widths,
-                              unnormalized_heights,
-                              unnormalized_derivatives,
-                              inverse=False,
-                              left=0., right=1., bottom=0., top=1.,
-                              min_bin_width=1e-3,
-                              min_bin_height=1e-3,
-                              min_derivative=1e-3):
-        
-        if torch.min(inputs) < left or torch.max(inputs) > right:
-           
-            raise Exception("outside boundaries in rational-spline flow! (min/max (%.2f/%.2f), allowed: (%.2f/%.2f)" % (torch.min(inputs), torch.max(inputs), left, right))
 
-        num_bins = unnormalized_widths.shape[-1]
-
-        if min_bin_width * num_bins > 1.0:
-            raise ValueError('Minimal bin width too large for the number of bins')
-        if min_bin_height * num_bins > 1.0:
-            raise ValueError('Minimal bin height too large for the number of bins')
-        
-        widths = F.softmax(unnormalized_widths, dim=-1)
-        widths = min_bin_width + (1 - min_bin_width * num_bins) * widths
-     
-        cumwidths = torch.cumsum(widths, dim=-1)
-        
-        cumwidths = F.pad(cumwidths, pad=(1, 0), mode='constant', value=0.0)
-        cumwidths = (right - left) * cumwidths + left
-        cumwidths[..., 0] = left
-        cumwidths[..., -1] = right
-        widths = cumwidths[..., 1:] - cumwidths[..., :-1]
-
-        derivatives = min_derivative + F.softplus(unnormalized_derivatives)
-
-        heights = F.softmax(unnormalized_heights, dim=-1)
-        heights = min_bin_height + (1 - min_bin_height * num_bins) * heights
-        cumheights = torch.cumsum(heights, dim=-1)
-        cumheights = F.pad(cumheights, pad=(1, 0), mode='constant', value=0.0)
-        cumheights = (top - bottom) * cumheights + bottom
-        cumheights[..., 0] = bottom
-        cumheights[..., -1] = top
-        heights = cumheights[..., 1:] - cumheights[..., :-1]
-
-       
-        
-        if inverse:
-            bin_idx = searchsorted(cumheights, inputs)#[..., None]
-        else:
-            bin_idx = searchsorted(cumwidths, inputs)#[..., None]
-        
-        if(cumwidths.shape[0]==1 and bin_idx.shape[0]>1):
-          
-          cumwidths=cumwidths.repeat(bin_idx.shape[0], 1)
-          widths=widths.repeat(bin_idx.shape[0], 1)
-          heights=heights.repeat(bin_idx.shape[0], 1)
-          cumheights=cumheights.repeat(bin_idx.shape[0], 1)
-          derivatives=derivatives.repeat(bin_idx.shape[0], 1)
-
-
-       
-        input_cumwidths = cumwidths.gather(-1, bin_idx)#[..., 0]
-
-        
-        input_bin_widths = widths.gather(-1, bin_idx)#[..., 0]
-
-        input_cumheights = cumheights.gather(-1, bin_idx)#[..., 0]
-        delta = heights / widths
-        input_delta = delta.gather(-1, bin_idx)#[..., 0]
-
-        input_derivatives = derivatives.gather(-1, bin_idx)#[..., 0]
-        input_derivatives_plus_one = derivatives[..., 1:].gather(-1, bin_idx)#[..., 0]
- 
-        input_heights = heights.gather(-1, bin_idx)#[..., 0]
-
-      
-        if inverse:
-            a = (((inputs - input_cumheights) * (input_derivatives
-                                                 + input_derivatives_plus_one
-                                                 - 2 * input_delta)
-                  + input_heights * (input_delta - input_derivatives)))
-            b = (input_heights * input_derivatives
-                 - (inputs - input_cumheights) * (input_derivatives
-                                                  + input_derivatives_plus_one
-                                                  - 2 * input_delta))
-            c = - input_delta * (inputs - input_cumheights)
-
-            discriminant = b.pow(2) - 4 * a * c
-            assert (discriminant >= 0).all()
-
-            root = (2 * c) / (-b - torch.sqrt(discriminant))
-            outputs = root * input_bin_widths + input_cumwidths
-
-            theta_one_minus_theta = root * (1 - root)
-            denominator = input_delta + ((input_derivatives + input_derivatives_plus_one - 2 * input_delta)
-                                         * theta_one_minus_theta)
-            derivative_numerator = input_delta.pow(2) * (input_derivatives_plus_one * root.pow(2)
-                                                         + 2 * input_delta * theta_one_minus_theta
-                                                         + input_derivatives * (1 - root).pow(2))
-            logabsdet = torch.log(derivative_numerator) - 2 * torch.log(denominator)
-
-            return outputs, -logabsdet
-        else:
-            theta = (inputs - input_cumwidths) / input_bin_widths
-            theta_one_minus_theta = theta * (1 - theta)
-
-            numerator = input_heights * (input_delta * theta.pow(2)
-                                         + input_derivatives * theta_one_minus_theta)
-            denominator = input_delta + ((input_derivatives + input_derivatives_plus_one - 2 * input_delta)
-                                         * theta_one_minus_theta)
-            outputs = input_cumheights + numerator / denominator
-
-            derivative_numerator = input_delta.pow(2) * (input_derivatives_plus_one * theta.pow(2)
-                                                         + 2 * input_delta * theta_one_minus_theta
-                                                         + input_derivatives * (1 - theta).pow(2))
-            logabsdet = torch.log(derivative_numerator) - 2 * torch.log(denominator)
-           
-            return outputs, logabsdet
 
     def _flow_mapping(self, inputs, extra_inputs=None): 
         
@@ -242,7 +120,7 @@ class rational_quadratic_spline(interval_base.interval_base):
             
 
        
-        x, log_det_update=self.rational_quadratic_spline(x, 
+        x, log_det_update=spline_fns.rational_quadratic_spline(x, 
                                                          widths, 
                                                          heights, 
                                                          derivatives, 
@@ -251,8 +129,8 @@ class rational_quadratic_spline(interval_base.interval_base):
                                                          right=self.high_boundary, 
                                                          bottom=self.low_boundary, 
                                                          top=self.high_boundary, 
-                                                         min_bin_width=self.min_width,
-                                                         min_bin_height=self.min_height,
+                                                         rel_min_bin_width=self.min_width,
+                                                         rel_min_bin_height=self.min_height,
                                                          min_derivative=self.min_derivative
                                                          )
         
@@ -269,6 +147,12 @@ class rational_quadratic_spline(interval_base.interval_base):
         heights=self.rel_log_heights.to(x)
         derivatives=self.rel_log_derivatives.to(x)
 
+        print(widths.shape)
+        print(heights.shape)
+        print(derivatives.shape)
+
+
+
         if(extra_inputs is not None):
 
             assert( (extra_inputs.shape[0]==x.shape[0]) or (extra_inputs.shape[0]==1) ), ("Extra inputs must be of shape B X .. (B=Batch size) or 1 X .. (broadcasting).. got for first dimension instead : ", extra_inputs.shape[0])
@@ -277,7 +161,11 @@ class rational_quadratic_spline(interval_base.interval_base):
             heights=extra_inputs[:,self.num_basis_elements:2*self.num_basis_elements]#.reshape(extra_inputs.shape[0], self.rel_log_heights.shape[1])
             derivatives=extra_inputs[:,2*self.num_basis_elements:]#.reshape(extra_inputs.shape[0], self.rel_log_derivatives.shape[1])
             
-        x, log_det_update=self.rational_quadratic_spline(x, 
+            print("used pars ... ")
+            print(widths.shape)
+            print(heights.shape)
+            print(derivatives.shape)
+        x, log_det_update=spline_fns.rational_quadratic_spline(x, 
                                                          widths, 
                                                          heights, 
                                                          derivatives, 
@@ -286,8 +174,8 @@ class rational_quadratic_spline(interval_base.interval_base):
                                                          right=self.high_boundary, 
                                                          bottom=self.low_boundary, 
                                                          top=self.high_boundary, 
-                                                         min_bin_width=self.min_width,
-                                                         min_bin_height=self.min_height,
+                                                         rel_min_bin_width=self.min_width,
+                                                         rel_min_bin_height=self.min_height,
                                                          min_derivative=self.min_derivative
                                                          )
        