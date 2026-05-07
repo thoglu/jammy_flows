6b2696f96fd2d496a9150a8ccf1c867ed91f5f6f
tg <tg>
2026-05-07 12:09:11 +0200

    add support for smooth rational quadratic splines

--- jammy_flows/layers/spheres/splines_1d.py
+++ jammy_flows/layers/spheres/splines_1d.py
@@ -3,18 +3,25 @@ from torch import nn
 import numpy
 
 from . import sphere_base
-from ..bisection_n_newton import inverse_bisection_n_newton
-from ..spline_fns import rational_quadratic_spline
-
-
+from .. import spline_fns
 
 class spline_1d(sphere_base.sphere_base):
     def __init__(self, dimension=1, 
                        euclidean_to_sphere_as_first=True, 
-                       add_rotation=1,
-                       natural_direction=0, 
+                       add_rotation=1, 
+                       natural_direction=1,
                        use_permanent_parameters=False,  
-                       num_basis_functions=10):
+                       num_basis_functions=2,
+                       min_width=1e-4, 
+                       min_height=1e-4, 
+                       min_derivative=1e-4,
+                       fix_boundary_derivatives=-1.0,
+                       smooth_second_derivative=0,
+                       #restrict_max_min_width_height_ratio=-1.0,
+                       fix_first_width_n_height_to_zero=0,
+                       also_fix_second_width_to_zero=0,
+                       independent_width_height_parametrization=0):
+
         """
         A circular spline variant. Symbol: "o"
 
@@ -25,119 +32,326 @@ class spline_1d(sphere_base.sphere_base):
         
         if(dimension!=1):
             raise Exception("The moebius flow is defined for dimension 1, but dimension %d is handed over" % (dimension))
-           
-        self.num_basis_functions=num_basis_functions
-
-        ## add parameters from this layer (on top of householder params defined in the class parent)
-        self.total_param_num+=self.num_basis_functions*3
-
-        if(use_permanent_parameters):
-            self.spline_pars=nn.Parameter(torch.randn(self.num_basis_functions,3).unsqueeze(0))
-            
-        ## natural direction means no bisection in the forward pass, but in the backward pass
+    
         self.natural_direction=natural_direction
+        self.fix_boundary_derivatives=fix_boundary_derivatives
 
-    def _inv_flow_mapping(self, inputs, extra_inputs=None, sf_extra=None):
+        self.num_basis_functions=num_basis_functions
+        self.num_width_params=num_basis_functions
+        self.num_height_params=num_basis_functions
 
-        [x,log_det]=inputs
+        self.fix_first_width_n_height_to_zero=fix_first_width_n_height_to_zero
+        self.also_fix_second_width_to_zero=also_fix_second_width_to_zero
 
-        if(extra_inputs is not None):
-            spline_pars=torch.reshape(extra_inputs, [-1, self.num_basis_functions, 3])
+        if(self.fix_first_width_n_height_to_zero):
+            self.num_width_params=num_basis_functions-1
+            self.num_height_params=num_basis_functions-1
+
+            ## second width has same length as first width
+            if(self.also_fix_second_width_to_zero>0):
+                self.num_width_params-=1
+
+            
+
+        ## widths of intervals in rational spline
+        if(use_permanent_parameters):
+            self.rel_log_widths=nn.Parameter(torch.randn(self.num_width_params).type(torch.double).unsqueeze(0))
+            
+        ## heights of intervals in rational spline
+        if(use_permanent_parameters):
+            self.rel_log_heights=nn.Parameter(torch.randn(self.num_height_params).type(torch.double).unsqueeze(0))
+        
+        ## derivatives
+       
+        self.deriv_num_bd_subtraction=0
+
+                
+        self.smooth_second_derivative=smooth_second_derivative
+        if(self.smooth_second_derivative==1):
+            assert( (self.num_basis_functions==2) ), "Only support 2 basis functions for smooth derivative!"
+            
+            self.deriv_num_bd_subtraction=3 # subtract all derivs.. all derivs will be predicted
+           
         else:
-            spline_pars=self.spline_pars.to(x)
+            if(self.fix_boundary_derivatives>0.0):
+                ## we only need to define this if we have no smooth derivatives
+                self.deriv_num_bd_subtraction=2
+                assert(self.fix_boundary_derivatives>min_derivative), "Fixed boundary derivative should be larger than min derivative!"
+
+                # boundary derivatives fixed value = 1 if used
+                self.boundary_log_derivs_fixed_value=numpy.log(numpy.exp(self.fix_boundary_derivatives-min_derivative)-1.0)
+
+            else:
+                self.deriv_num_bd_subtraction=1 # if we fit boundary derivative, it must be the same at 0 and 2pi so subtract one
+
+        self.num_derivative_params=num_basis_functions+1-self.deriv_num_bd_subtraction
+
+        if(self.smooth_second_derivative):
+            if(self.num_basis_functions==3):
+                ## subtract another width/height pair as we are in a symmetric situation
+                self.num_width_params-=1
+                self.num_height_params-=1
+                
+        ## only use derivatives if necessary
+        if(use_permanent_parameters):
+            if( self.num_derivative_params>0):
+                self.rel_log_derivatives=nn.Parameter(torch.randn(self.num_derivative_params).type(torch.double).unsqueeze(0))
+        
+        ## subtract number of derivative parameters as we need
+        self.total_param_num+=self.num_width_params+self.num_height_params+self.num_derivative_params #3*self.num_basis_functions+1-self.deriv_num_bd_subtraction
+
+        ## minimum values to which relative logarithmic values are added with softmax
+     
+        self.min_width=min_width
+        self.min_height=min_height
+        self.min_derivative=min_derivative
+       
+        self.independent_width_height_parametrization=independent_width_height_parametrization
+
+    def _inv_flow_mapping(self, inputs, extra_inputs=None, extra_inputs_base=None, sf_extra=None):
+
+        [x, log_det]=inputs
 
         if(self.always_parametrize_in_embedding_space):
             # embedding to intrinsic
             x, log_det=self.eucl_to_spherical_embedding(x, log_det)
 
-        # mirror derivatives at the endpoints
-        derivs=torch.cat([spline_pars[:,:,2], spline_pars[:,0:1,2]], dim=1)
+        ## make sure we stay in range
+        x=spline_fns.return_safe_angle_within_2pi(x)
+        #x=torch.where(x>=2*numpy.pi, 2*numpy.pi, x)
+        #x=torch.where(x<0.0, 0.0, x)
+
+        if(self.use_permanent_parameters):
+            widths=self.rel_log_widths.to(x)
+            heights=self.rel_log_heights.to(x)
+            derivatives=None
+            if(self.num_derivative_params>0):
+                derivatives=self.rel_log_derivatives.to(x)
+
+        else:
+            
+            assert( extra_inputs is not None), "Conditional PDF.. require *extra_inputs*"
+            assert( (extra_inputs.shape[0]==x.shape[0]) or (extra_inputs.shape[0]==1) ), ("Extra inputs must be Tensor of shape B X .. (B=Batch size) or 1 X .. (broadcasting).. got for first dimension instead : ", extra_inputs.shape[0])
+
+            widths=extra_inputs[:,:self.num_width_params]#.reshape(extra_inputs.shape[0], self.rel_log_widths.shape[1])
+            heights=extra_inputs[:,self.num_width_params:self.num_width_params+self.num_height_params]#.reshape(extra_inputs.shape[0], self.rel_log_heights.shape[1])
+            
+            if(self.num_derivative_params>0):
+                derivatives=extra_inputs[:,self.num_width_params+self.num_height_params:]#.reshape(extra_inputs.shape[0], self.rel_log_derivatives.shape[1])
+        
+        ## check if have first width/height fixed to 0
+        if(self.fix_first_width_n_height_to_zero):
+
+            zero_add=torch.zeros_like(heights[:,0:1])
+
+            heights=torch.cat([zero_add, heights], dim=1)
+
+            if(self.also_fix_second_width_to_zero):
+                widths=torch.cat([zero_add, zero_add, widths], dim=1)
+            else:
+                widths=torch.cat([zero_add, widths], dim=1)
+
+        ## check for relative scaling
+        if(self.independent_width_height_parametrization):
+            heights=widths+heights
 
         use_inverse=True
         if(self.natural_direction==0):
             use_inverse=False
 
-        x, log_deriv=rational_quadratic_spline(x,
-                      spline_pars[:,:,0],
-                      spline_pars[:,:,1],
-                      derivs,
-                      inverse=use_inverse,
-                      left=0, right=2*numpy.pi, bottom=0, top=2*numpy.pi,
-                      rel_min_bin_width=1e-3,
-                      rel_min_bin_height=1e-3,
-                          min_derivative=1e-3)
+        if(self.smooth_second_derivative==0):
 
-      
-        log_deriv=log_deriv.sum(axis=-1)
-        
-        log_det=log_det+log_deriv
+            if(self.fix_boundary_derivatives>0.0):
+                first_and_last=torch.ones(derivatives.shape[:-1]+(1,)).to(x)*self.boundary_log_derivs_fixed_value
 
+                derivatives=torch.cat([first_and_last, derivatives, first_and_last], dim=-1)
+            else:
+                ## copy over first element to the end
+                derivatives=torch.cat([derivatives, derivatives[:,0:1]], dim=-1)
+
+            x, log_det_update=spline_fns.rational_quadratic_spline(x, 
+                                                             widths, 
+                                                             heights, 
+                                                             derivatives, 
+                                                             inverse=use_inverse, 
+                                                             left=0.0, 
+                                                             right=2*numpy.pi, 
+                                                             bottom=0.0, 
+                                                             top=2*numpy.pi, 
+                                                             rel_min_bin_width=self.min_width,
+                                                             rel_min_bin_height=self.min_height,
+                                                             min_derivative=self.min_derivative
+                                                             )
+        else:
+
+            x, log_det_update=spline_fns.rational_quadratic_spline_smooth_circular(x, 
+                                                             widths, 
+                                                             heights, 
+                                                             inverse=use_inverse, 
+                                                             rel_min_bin_width=self.min_width,
+                                                             rel_min_bin_height=self.min_height,
+                                                             shift_to_middle=True
+                                                             )
+       
+        log_det_new=log_det+log_det_update.sum(axis=-1)
         
+        ## make sure we stay in range
+        x=spline_fns.return_safe_angle_within_2pi(x)
+        #x=torch.where(x>=2*numpy.pi, 2*numpy.pi, x)
+        #x=torch.where(x<0.0, 0.0, x)
+
         if(self.always_parametrize_in_embedding_space):
             # embedding to intrinsic
             x, log_det=self.spherical_to_eucl_embedding(x, log_det)
 
-        
-        return x, log_det
+        return x, log_det_new
 
-    def _flow_mapping(self, inputs, extra_inputs=None, sf_extra=None):
-        
-       
-        [x,log_det]=inputs
 
+    def _flow_mapping(self, inputs, extra_inputs=None, extra_inputs_base=None, sf_extra=None):
         
-        if(extra_inputs is not None):
-            spline_pars=torch.reshape(extra_inputs, [-1, self.num_basis_functions, 3])
-        else:
-            spline_pars=self.spline_pars.to(x)
         
+        [x, log_det]=inputs
+
         if(self.always_parametrize_in_embedding_space):
             # embedding to intrinsic
             x, log_det=self.eucl_to_spherical_embedding(x, log_det)
+
+        ## make sure we stay in range
+        x=torch.where(x>=2*numpy.pi, 2*numpy.pi, x)
+        x=torch.where(x<0.0, 0.0, x)
+
+        if(self.use_permanent_parameters):
+            widths=self.rel_log_widths.to(x)
+            heights=self.rel_log_heights.to(x)
+            derivatives=None
+            if(self.num_derivative_params>0):
+                derivatives=self.rel_log_derivatives.to(x)
+
+        else:
+            
+            assert( extra_inputs is not None), "Conditional PDF.. require *extra_inputs*"
+            assert( (extra_inputs.shape[0]==x.shape[0]) or (extra_inputs.shape[0]==1) ), ("Extra inputs must be Tensor of shape B X .. (B=Batch size)", x.shape[0], "or 1 X .. (broadcasting).. got for first dimension instead : ", extra_inputs.shape[0])
+
+            widths=extra_inputs[:,:self.num_width_params]#.reshape(extra_inputs.shape[0], self.rel_log_widths.shape[1])
+            heights=extra_inputs[:,self.num_width_params:self.num_width_params+self.num_height_params]#.reshape(extra_inputs.shape[0], self.rel_log_heights.shape[1])
+            
+            if(self.num_derivative_params>0):
+                derivatives=extra_inputs[:,self.num_width_params+self.num_height_params:]#.reshape(extra_inputs.shape[0], self.rel_log_derivatives.shape[1])
         
-        # mirror derivatives at the endpoints
-        derivs=torch.cat([spline_pars[:,:,2], spline_pars[:,0:1,2]], dim=1)
+        if(self.fix_first_width_n_height_to_zero):
+
+            zero_add=torch.zeros_like(heights[:,0:1])
+
+            heights=torch.cat([zero_add, heights], dim=1)
+
+            if(self.also_fix_second_width_to_zero):
+                widths=torch.cat([zero_add, zero_add, widths], dim=1)
+            else:
+                widths=torch.cat([zero_add, widths], dim=1)
+
+        ## check for relative scaling
+        if(self.independent_width_height_parametrization):
+
+            heights=widths+heights
 
         use_inverse=False
         if(self.natural_direction==0):
             use_inverse=True
 
-        x, log_deriv=rational_quadratic_spline(x,
-                      spline_pars[:,:,0],
-                      spline_pars[:,:,1],
-                      derivs,
-                      inverse=use_inverse,
-                      left=0, right=2*numpy.pi, bottom=0, top=2*numpy.pi,
-                      rel_min_bin_width=1e-3,
-                      rel_min_bin_height=1e-3,
-                          min_derivative=1e-3)
+        if(self.smooth_second_derivative==0):
 
-        log_deriv=log_deriv.sum(axis=-1)
+            if(self.fix_boundary_derivatives>0.0):
 
-        log_det=log_det+log_deriv
+                first_and_last=torch.ones(derivatives.shape[:-1]+(1,)).to(x)*self.boundary_log_derivs_fixed_value
+
+                derivatives=torch.cat([first_and_last, derivatives, first_and_last], dim=-1)
+            else:
+                ## copy over first element to the end
+                derivatives=torch.cat([derivatives, derivatives[:,0:1]], dim=-1)
+
+            x, log_det_update=spline_fns.rational_quadratic_spline(x, 
+                                                             widths, 
+                                                             heights, 
+                                                             derivatives, 
+                                                             inverse=use_inverse, 
+                                                             left=0.0, 
+                                                             right=2*numpy.pi, 
+                                                             bottom=0.0, 
+                                                             top=2*numpy.pi, 
+                                                             rel_min_bin_width=self.min_width,
+                                                             rel_min_bin_height=self.min_height,
+                                                             min_derivative=self.min_derivative
+                                                             )
+        else:
+
+            x, log_det_update=spline_fns.rational_quadratic_spline_smooth_circular(x, 
+                                                             widths, 
+                                                             heights, 
+                                                             inverse=use_inverse, 
+                                                             rel_min_bin_width=self.min_width,
+                                                             rel_min_bin_height=self.min_height,
+                                                             shift_to_middle=True
+                                                             )
+       
+        log_det_new=log_det+log_det_update.sum(axis=-1)
+
+        ## make sure we stay in range
+        x=torch.where(x>=2*numpy.pi, 2*numpy.pi, x)
+        x=torch.where(x<0.0, 0.0, x)
 
         if(self.always_parametrize_in_embedding_space):
             # embedding to intrinsic
             x, log_det=self.spherical_to_eucl_embedding(x, log_det)
-
-        return x, log_det
-
-    def _init_params(self, params):
-
-        self.spline_pars.data=params.reshape(1, self.num_basis_functions, 3)
+        
+        return x, log_det_new
 
     def _get_desired_init_parameters(self):
 
-        ## fixed params for the spline
-        return torch.ones((self.num_basis_functions*3))*0.54
+        ## widths
+
+        desired_param_vec=[]
+
+        ## 0.54 as start value in log space seems to result in a rather flat spline (defined by the log-derivatives in particular)
+        if(self.smooth_second_derivative):  
+            desired_param_vec.append(torch.zeros(self.num_width_params+self.num_height_params+self.num_derivative_params))
+        else:
+            desired_param_vec.append(torch.ones(self.num_width_params+self.num_height_params+self.num_derivative_params)*0.54)
+        
+        return torch.cat(desired_param_vec)
+
+    def _init_params(self, params):
+
+        counter=0
+        self.rel_log_widths.data[0,:]=params[counter:counter+self.num_width_params]    
+        counter+=self.num_width_params
+        
+        self.rel_log_heights.data[0,:]=params[counter:counter+self.num_height_params]
+        counter+=self.num_height_params
+
+        if(self.num_derivative_params>0):
+            self.rel_log_derivatives.data[0,:]=params[counter:counter+self.num_derivative_params]
 
     def _obtain_layer_param_structure(self, param_dict, extra_inputs=None, previous_x=None, extra_prefix=""): 
-     
-        if(extra_inputs is not None):
-            spline_pars=extra_inputs.reshape(-1, self.num_basis_functions, 3)
-        else:
-            spline_pars=self.spline_pars
 
-        param_dict[extra_prefix+"moebius"]=spline_pars.data
+        extra_input_counter=0
+
+        if(self.use_permanent_parameters):
+            widths=self.rel_log_widths
+            heights=self.rel_log_heights
+            if(self.num_derivative_params>0):
+                derivatives=self.rel_log_derivatives
+        else:
+            assert(extra_inputs is not None)
+            
+            widths=extra_inputs[:,:self.num_width_params]
+            heights=extra_inputs[:,self.num_width_params:self.num_width_params+self.num_height_params]
+            if(self.num_derivative_params>0):
+                derivatives=extra_inputs[:,self.num_width_params+self.num_height_params:]
+
+        param_dict[extra_prefix+"widths"]=widths
+        param_dict[extra_prefix+"heights"]=heights
+
+        if(self.smooth_second_derivative==0):
+            param_dict[extra_prefix+"derivatives"]=derivatives
         
 
