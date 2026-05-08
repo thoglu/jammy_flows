fix rotation param issue without added rotation and test

jammy_flows/layers/spheres/sphere_base.py
@@ -72,6 +72,7 @@ class sphere_base(layer_base.layer_base):
         self.rotation_mode=rotation_mode
         self.add_rotation=add_rotation
 
+        ## houeholder params stands for any rotation params here
         self.num_householder_params=0
 
         if(self.add_rotation):
@@ -103,7 +104,8 @@ class sphere_base(layer_base.layer_base):
                 self.num_householder_params=self.num_householder_iter*(dimension+1)
                 
         if(use_permanent_parameters):
-            self.householder_params=nn.Parameter(torch.randn((1, self.num_householder_params  )))
+            if(self.num_householder_params>0):
+                self.householder_params=nn.Parameter(torch.randn((1, self.num_householder_params  )))
 
         self.total_param_num+=self.num_householder_params
 

tests/test_manifold_embedding_consistency.py
@@ -104,7 +104,7 @@ def setup_and_test_multiple_pdfs(pdf_def, layer_def):
         intrinsic_forced_embedded_eval,_,_=intrinsic_pdf(embedded_sample, force_embedding_coordinates=True)
       
         for ind in range(1):
-            
+
             res1=torch.autograd.grad(normal_eval[ind], normal_pdf.parameters(), allow_unused=False, retain_graph=True)[0]
             res2=torch.autograd.grad(embedded_eval[ind], embedded_pdf.parameters(), allow_unused=False, retain_graph=True)[0]
 